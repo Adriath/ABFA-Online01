eefd0d0e996403b92b94eaa37f55727b70ad9a67
adria <adria@WassPC>
2020-10-18 23:43:53 +0200

    Subida de la tarea online 1

---
+++ src/TareaOnline1.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author adria
+ */
+public class TareaOnline1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        int variable=100;
+        String nombre="Adrián Arjona";
+
+        System.out.println("Mi nombre es "  + nombre);
+        System.out.println("\n Para crear una variable debemos usar int"
+        + "\n para indicar que es un número entero y a continuación"
+        + "\n iniciarla con el valor, 100 en este caso.");
+        System.out.println("\n El ejemplo es: \n int variable=" + variable);
+        
+    }
+    
+}
